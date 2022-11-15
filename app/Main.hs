995df3b1607fb5e34a43eb07f86877b8bb996897
Iandmee <milaevpro1@gmail.com>
2022-11-15 18:17:19 +0200

    3 methods work, without OOP

--- app/Main.hs
+++ app/Main.hs
@@ -7,3 +7,5 @@ f x = Just (x^2)
 main :: IO()
 main = do 
   putStrLn $ show $ partApproxReactangles f 1.0 2.0 0.001
+  putStrLn $ show $ partApproxTrap f 1.0 2.0 0.001
+  putStrLn $ show $ partApproxSimpson f 1.0 2.0 0.001
--- src/Integration.hs
+++ src/Integration.hs
@@ -18,11 +18,27 @@ iterateThroughPartition calculateByXXX f a b d initIntegration lastIntegration p
 calculateByReactangles :: (Double -> Maybe Double) -> Double -> Double -> Double -> Int -> Either Error Double
 calculateByReactangles f a step ans 0  = Right ans
 calculateByReactangles f a step ans parts = do 
-  let result = f (a + (step / 2.0))
+  let result = f (a + (step / 2))
   case result of
   	Nothing -> Left InputFunctionOrBoundsError
   	Just res -> calculateByReactangles f (a + step) step (ans + res * step) (parts - 1)
 
+calculateByTrap :: (Double -> Maybe Double) -> Double -> Double -> Double -> Int -> Either Error Double
+calculateByTrap f a step ans 1  = Right ans
+calculateByTrap f a step ans parts = do 
+  let result = f (a + step)
+  case result of
+    Nothing -> Left InputFunctionOrBoundsError
+    Just res -> calculateByTrap f (a + step) step (ans + res * step) (parts - 1)
+
+calculateBySimpson :: (Double -> Maybe Double) -> Double -> Double -> Double -> Int -> Either Error Double
+calculateBySimpson f a step ans 0  = Right ans
+calculateBySimpson f a step ans parts = do 
+  let trap2 = calculateByTrap f a (step / 2) (ans / 2) (parts * 2)
+  let trap1 = calculateByTrap f a step ans parts
+  case (trap1, trap2) of 
+    (Right t1, Right t2) -> Right ((4 * t2 - t1) / 3)
+    otherwise -> Left InputFunctionOrBoundsError 
 
 partApproxReactangles :: (Double -> Maybe Double) -> Double -> Double -> Double -> Either Error Double
 partApproxReactangles f a b d | d == 0.0 = Left NullSizeOfError
@@ -30,6 +46,20 @@ partApproxReactangles f a b d | d == 0.0 = Left NullSizeOfError
 
 partApproxTrap :: (Double -> Maybe Double) -> Double -> Double -> Double -> Either Error Double
 partApproxTrap f a b d | d == 0.0 = Left NullSizeOfError
-                   | otherwise = iterateThroughPartition calculateByReactangles f a b d 0.0 Nothing 1
+                       | f a == Nothing = Left InputFunctionOrBoundsError
+                       | f b == Nothing = Left InputFunctionOrBoundsError
+                       | otherwise = iterateThroughPartition calculateByTrap f a b d initIntegration Nothing 1
+                   where
+                     initIntegration = case ((f a), (f b)) of 
+                       (Just y1, Just y2) -> (y1 + y2) / 2
+
+partApproxSimpson :: (Double -> Maybe Double) -> Double -> Double -> Double -> Either Error Double
+partApproxSimpson f a b d | d == 0.0 = Left NullSizeOfError
+                          | f a == Nothing = Left InputFunctionOrBoundsError
+                          | f b == Nothing = Left InputFunctionOrBoundsError
+                          | otherwise = iterateThroughPartition calculateBySimpson f a b d initIntegration Nothing 1
+                      where
+                        initIntegration = case ((f a), (f b)) of 
+                          (Just y1, Just y2) -> (y1 + y2) / 2
 
 
